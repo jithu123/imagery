ae136373bbee3c1797c9a083ddc1e79e008cfcaf
Jithendranath Joijoide <jithendr@oath.com>
2018-03-22 19:13:06 -0700

    Initial Commit

---
+++ imagery/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  imagery
+//
+//  Created by Jithendranath Joijoide on 3/22/18.
+//  Copyright © 2018 Jithendranath Joijoide. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
